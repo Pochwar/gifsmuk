ca2802d8c51e8042945c14c6574bbee5b6ad2a06
Benoît Ripoche <benoit.ripoche@circularx.com>
2023-10-14 00:27:37 +0200

    poc implement instagram

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,31 @@
-import App from './App';
-require('dotenv').config();
+import TwitterApp from './TwitterApp';
+import IGApp from './IGApp';
+// require('dotenv').config();
 
-if (
-  process.env.TWITTER_CONSUMER_KEY === undefined
-  || process.env.TWITTER_CONSUMER_SECRET === undefined
-  || process.env.TWITTER_ACCESS_TOKEN_KEY === undefined
-  || process.env.TWITTER_ACCESS_TOKEN_SECRET === undefined
-) {
-  console.log('Please provide Twitter\'s Consumer Key, Consumer Secret, Access Token Key & Access Token Secret in .env file')
-} else {
-  const app = new App(
-    process.env.TWITTER_CONSUMER_KEY,
-    process.env.TWITTER_CONSUMER_SECRET,
-    process.env.TWITTER_ACCESS_TOKEN_KEY,
-    process.env.TWITTER_ACCESS_TOKEN_SECRET
-  );
+// Get keyword from command line
+const client = process.argv[2];
 
+let app;
+
+switch (client) {
+  case 'instagram':
+    app = new IGApp(process.env);
+    break;
+  case 'twitter':
+    app = new TwitterApp(process.env);
+    break;
+  default:
+    console.log('Invalid client')
+    break;
+}
+
+if (app) {
   // Get keyword from command line
-  const keyword = process.argv[2];
+  const keyword = process.argv[3];
 
 // Send error message if keyword is undefined
   if (keyword === undefined) {
-    console.log('Please provide a search keyword. Use the command "npm start keyword"')
+    console.log('Please provide a search keyword. Use the command "npm start <client> <keyword>"')
   } else {
     app.run(keyword);
   }
